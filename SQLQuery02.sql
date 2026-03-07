141dabd22c706443fb0700e63544e348337d4652
Anon3605 <arafatgovt01@gmail.com>
2026-03-08 00:07:17 +0600

    Commit 01

--- SQLQuery02.sql
+++ SQLQuery02.sql
@@ -5,19 +5,29 @@ from employees
 where salary>90000 and is_active=1
 
 --Task 2: Find products with a unit_price BETWEEN 20,000 and 100,000. Then find products in categories 1, 4, or 5 only.
-
+select product_name, unit_price, cat_id
+from products
+where cat_id in (1,4,5) and unit_price between 20000 and 100000
 
 --Task 3: Find all employees whose email ends with @acme.com AND whose first name starts with 'S'.
-
+select * from employees
+where email like '%@acme.com' and first_name like 'S%'
 
 --Task 4.1: What is the total salary payroll per department?
-
+select dept_id,sum(salary) 
+from employees
+group by dept_id
 
 --Task 4.2: What is the average product price per category?
-
+select cat_id, avg(unit_price)
+from products
+group by cat_id
 
 --Task 4.3: How many employees does each department have?
-
+select dept_id, count(emp_id) as total_employee
+from employees
+group by dept_id
+order by total_employee asc
 
 --Task 5: Find departments where the total salary bill exceeds 300,000. Show dept_id and the total.
 
